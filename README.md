d07559d2e879996aa24d7c2057f58c5573c927dd
iamkr <ketanravindra.mahajan@aircanada.ca>
2019-10-10 16:45:41 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # microservices
-Test code samples 
+auth server: 9191
+profile service: 9090
+eureka: 8761
+movie catelog: 8989
+movie info: 8888
+ratings data: 8787
+
+
+Coverage:
+1. Shared models between all services
+2. Service Discovery
+3. Fault Tolerance & resilence 
+--Request Timeout
+--Circuit Breaker Mechanism
+--Fallback Pattern
+--Bulk Head Pattern Implementation
+--Dashboard for API performance Monitoring
+4. Swagger for API documentation
+5. Authentication & Authorisation
+6. SSO
+7. Reactive REST support for future
+8. Automated Test framework
+
+
+
+
---
+++ profile-service/src/main/java/com/kr/profile/ProfileServiceApplication.java
@@ -0,0 +1,25 @@
+package com.kr.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.kr.models.commons.model")
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableSwagger2
+@EnableEurekaClient
+public class ProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileServiceApplication.class, args);
+    }
+
+}
+
