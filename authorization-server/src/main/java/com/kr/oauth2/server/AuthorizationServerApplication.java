7e399470af745de2ca65e91fe312ab076ae8e857
iamkr <ketanravindra.mahajan@aircanada.ca>
2019-11-05 10:43:15 +0530

    auth server deployment ready

--- authorization-server/src/main/java/com/kr/oauth2/server/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/kr/oauth2/server/AuthorizationServerApplication.java
@@ -2,12 +2,12 @@ package com.kr.oauth2.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-@EnableEurekaClient
+//@EnableEurekaClient
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
